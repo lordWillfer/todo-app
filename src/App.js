805e4217ce9d3eb40f420db25f8d6c3f5ae1dd1d
Williams E. Fernández <williamsenrique@gmail.com>
2023-08-02 11:22:26 -0600

    Ejemplos de creación de componentes simples en React

--- src/App.js
+++ src/App.js
@@ -6,6 +6,30 @@ class App extends Component {
     return (
       <div className="App">
         My Hello World!
+        <FirstComponent/>
+        <SecondComponent/>
+      </div>
+    );
+  }
+}
+
+// Class First Component
+class FirstComponent extends Component {
+  render() {
+    return (
+      <div className="firstComponent">
+        First Component
+      </div>
+    );
+  }
+}
+
+// Class Second Component
+class SecondComponent extends Component {
+  render() {
+    return (
+      <div className="secondComponent">
+        Second Component
       </div>
     );
   }
