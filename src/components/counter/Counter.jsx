c1ee3c3e8b8ab583fac281b79f9038d02e6aee4a
Williams E. Fernández <williamsenrique@gmail.com>
2023-08-11 17:21:49 -0600

    Pasando parámetros y llamadas a métodos entre componentes

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -4,15 +4,36 @@ import './Counter.css';
 
 // Class Counter Component
 class Counter extends Component {
+
+    // Definir el estado inicial en un constructor
+    // state => counter 0
+    constructor() {
+        super();
+
+        this.state = {
+            counter: 0
+        }
+
+        this.increment = this.increment.bind(this);
+    }
+
     render() {
         return (
-          <div className="counter">
-            <CounterButton />
-            <CounterButton by={5} />
-            <CounterButton by={10} />
-          </div>
+            <div className="counter">
+                <CounterButton incrementMethod={this.increment} />
+                <CounterButton by={5} incrementMethod={this.increment} />
+                <CounterButton by={10} incrementMethod={this.increment} />
+                <span className="count">{this.state.counter}</span>
+            </div>
         );
-      }
+    }
+
+    increment(by) {
+        //console.log(`Increment from child - ${by}`)
+        this.setState({
+            counter: this.state.counter + by
+        });
+    }
 }
 
 // Class CounterButton Component
@@ -22,7 +43,7 @@ class CounterButton extends Component {
     // state => counter 0
     constructor() {
         super();// Error 1
-        
+
         this.state = {
             counter: 0
         }
@@ -34,26 +55,25 @@ class CounterButton extends Component {
         return (
             <div className="counter">
                 <button onClick={this.increment}>+{this.props.by}</button>
-                <span className="count">{this.state.counter}</span>
             </div>
         );
     }
 
     increment() {
-        // Actualizar el estado - counter++
-        //console.log('Increment');
         this.setState({
             counter: this.state.counter + this.props.by
         });
+
+        this.props.incrementMethod(this.props.by);
     }
 }
 
 CounterButton.defaultProps = {
-    by : 1
+    by: 1
 }
 
 CounterButton.propTypes = {
-    by : PropTypes.number
+    by: PropTypes.number
 }
 
 export default Counter;
