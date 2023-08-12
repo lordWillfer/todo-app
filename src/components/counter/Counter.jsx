cee99c07dc1168e8d26ce172b6c1cd9f14a23578
Williams E. Fernández <williamsenrique@gmail.com>
2023-08-11 18:10:24 -0600

    Llamando directamente a las funciones del componente padre

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -22,11 +22,12 @@ class Counter extends Component {
     render() {
         return (
             <div className="counter">
-                <CounterButton incrementMethod={this.increment} decrementMethod={this.decrement} />
-                <CounterButton by={5} incrementMethod={this.increment} decrementMethod={this.decrement} />
+                <CounterButton by={100} incrementMethod={this.increment} decrementMethod={this.decrement} />
                 <CounterButton by={10} incrementMethod={this.increment} decrementMethod={this.decrement} />
+                <CounterButton by={2} incrementMethod={this.increment} decrementMethod={this.decrement} />
+                <CounterButton incrementMethod={this.increment} decrementMethod={this.decrement} />
                 <span className="count">{this.state.counter}</span>
-                <div><button className="reset" onClick={this.reset}>Reset</button></div>
+                <div><button className="reset" onClick={this.reset}>RESET</button></div>
             </div>
         );
     }
@@ -57,46 +58,13 @@ class Counter extends Component {
 // Class CounterButton Component
 class CounterButton extends Component {
 
-    // Definir el estado inicial en un constructor
-    // state => counter 0
-    constructor() {
-        super();// Error 1
-
-        this.state = {
-            counter: 0
-        }
-
-        this.increment = this.increment.bind(this);
-        this.decrement = this.decrement.bind(this);
-    }
-
     render() {
         return (
             <div className="counter">
-                <button onClick={this.increment}>+{this.props.by}</button>
-                <button onClick={this.decrement}>-{this.props.by}</button>
+                <button onClick={() => this.props.incrementMethod(this.props.by)}>+{this.props.by}</button>
+                <button onClick={() => this.props.decrementMethod(this.props.by)}>-{this.props.by}</button>
             </div>
         );
-    }
-
-    increment() {
-        this.setState(
-            () => {
-                return { counter: this.state.counter + this.props.by }
-            }
-        );
-
-        this.props.incrementMethod(this.props.by);
-    }
-
-    decrement() {
-        this.setState(
-            () => {
-                return { counter: this.state.counter - this.props.by }
-            }
-        );
-
-        this.props.decrementMethod(this.props.by);
     }
 }
 
