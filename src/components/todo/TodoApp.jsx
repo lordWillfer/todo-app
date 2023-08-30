db8d5fbd8453c0a32aa104400a69c9ffb32e385b
Williams E. Fernández <williamsenrique@gmail.com>
2023-08-30 15:11:37 -0600

    Personalizando la barra de encabezado y agregando Bootstrap al proyecto

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -12,6 +12,7 @@ class TodoApp extends Component {
         return (
             <div className="TodoApp">
                 <Router>
+                    <HeaderComponent />
                     <Routes>
                             <Route path="/" element={<LoginComponentWithNavigation />} />
                             <Route path="/login" element={<LoginComponentWithNavigation />} />
@@ -19,7 +20,38 @@ class TodoApp extends Component {
                             <Route path="/todos" element={<ListTodosComponent />} />
                             <Route path="*" element={<ErrorComponent />} />
                     </Routes>
+                    <FooterComponent />
                 </Router>
+            </div>
+        )
+    }
+}
+
+class HeaderComponent extends Component {
+    render() {
+        return (
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div><a href="/welcome" className="navbar-brand">My TODO Application</a></div>
+                    <ul className="navbar-nav">
+                        <li><Link className="nav-link" to="/welcome/lordWillfer">Home</Link></li>
+                        <li><Link className="nav-link" to="/todos">Todos</Link></li>
+                    </ul>
+                    <ul className="navbar-nav navbar-collapse justify-content-end">
+                        <li><Link className="nav-link" to="/login">Login</Link></li>
+                        <li><Link className="nav-link" to="/logout">Logout</Link></li>
+                    </ul>
+                </nav>
+            </header>
+        )
+    }
+}
+
+class FooterComponent extends Component {
+    render() {
+        return (
+            <div>
+                <hr />Footer
             </div>
         )
     }
